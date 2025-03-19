0cac6fd7d51c086ca52235cc78e0dae8839c1c61
ha0td4 <tienducanhhao@gmail.com>
2025-03-19 10:10:42 +0000

    easier to use with parsing arguments

--- NTLFlowLyzer/__main__.py
+++ NTLFlowLyzer/__main__.py
@@ -2,12 +2,14 @@
 
 import argparse
 import glob
+import json
+import os
 
 from NTLFlowLyzer.config_loader import ConfigLoader
 from .network_flow_analyzer import NTLFlowLyzer
 
 def args_parser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser(prog='NTLFlowLyzer')
+    parser = argparse.ArgumentParser(prog='ntlflowlyzer', description='Network Traffic Flow Analyzer')
     parser.add_argument('-c', '--config-file', action='store', help='Json config file address.')
     parser.add_argument('-o', '--online-capturing', action='store_true',
                         help='Capturing mode. The default mode is offline capturing.')
@@ -16,6 +18,12 @@ def args_parser() -> argparse.ArgumentParser:
     parser.add_argument('-cb', '--continues-batch-mode', action='store_true',
                         help='Continues batch mode. Analyze files in the given directory continuously.'
                             ' Default is False.')
+    parser.add_argument('-f', '--pcap-file', action='store',
+                        help='Path to specific PCAP file to analyze.')
+    parser.add_argument('--csv', action='store',
+                        help='Path for output CSV file. (default = ntlflowlyer-output.csv)', default='ntlflowlyer-output.csv')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='Enable verbose output.')
     return parser
 
 
@@ -26,28 +34,33 @@ def find_pcap_files(directory):
 
 
 def main():
-    print("You initiated NTLFlowLyzer!")
     parsed_arguments = args_parser().parse_args()
-    config_file_address = "./NTLFlowLyzer/config.json" if parsed_arguments.config_file is None else parsed_arguments.config_file
-    online_capturing = parsed_arguments.online_capturing
+    if len(vars(parsed_arguments)) == 0:
+        args_parser().print_help()
+        return
+
     if not parsed_arguments.batch_mode:
-        config = ConfigLoader(config_file_address)
-        network_flow_analyzer = NTLFlowLyzer(config, online_capturing, parsed_arguments.continues_batch_mode)
+        config = ConfigLoader(parsed_arguments.config_file, parsed_arguments.pcap_file, parsed_arguments.csv)
+        # print all values in config
+        print(">> Configurations:")
+        for key, value in vars(config).items():
+            print(f"{key}: {value}")
+        network_flow_analyzer = NTLFlowLyzer(config, parsed_arguments.online_capturing, parsed_arguments.continues_batch_mode)
         network_flow_analyzer.run()
         return
 
-    print(">> Batch mode is on!")
-    config = ConfigLoader(config_file_address)
+    print(">> Batch mode is on.")
+    config = ConfigLoader(parsed_arguments.config_file)
     batch_address = config.batch_address
     batch_address_output = config.batch_address_output
     pcap_files = find_pcap_files(batch_address)
-    print(f">> {len(pcap_files)} number of files detected. Lets go for analyze them!")
+    print(f">> {len(pcap_files)} number of files detected in the batch directory.")
     for file in pcap_files:
         print(100*"#")
         output_file_name = file.split('/')[-1]
         config.pcap_file_address = file
         config.output_file_address = f"{batch_address_output}/{output_file_name}.csv"
-        network_flow_analyzer = NTLFlowLyzer(config, online_capturing, parsed_arguments.continues_batch_mode)
+        network_flow_analyzer = NTLFlowLyzer(config, parsed_arguments.online_capturing, parsed_arguments.continues_batch_mode)
         network_flow_analyzer.run()
 
 
--- NTLFlowLyzer/config_loader.py
+++ NTLFlowLyzer/config_loader.py
@@ -4,40 +4,43 @@ import json
 import multiprocessing
 
 class ConfigLoader:
-    def __init__(self, config_file_address: str):
-        self.config_file_address = config_file_address
-        self.pcap_file_address: str = None
-        self.output_file_address: str = "./"
+    def __init__(self, config_file_address: str = None, 
+                 pcap_file_address: str = None,
+                 output_file_address: str = None):
+        self.batch_address: str = None
+        self.vxlan_ip: str = None
+        self.continues_batch_address: str = None
+        self.continues_pcap_prefix: str = None
+        self.batch_address_output: str = None
+        self.number_of_continues_files: int = 0
+        self.label: str = "Unknown"
+        self.protocols: list = []
         self.interface_name: str = "eth0"
         self.max_flow_duration: int = 120000
         self.activity_timeout: int = 5000
-        self.protocols: list = []
         self.floating_point_unit: str = ".4f"
         self.features_ignore_list: list = []
         self.number_of_threads: int = multiprocessing.cpu_count()
-        self.label = "Unknown"
-        self.feature_extractor_min_flows = 4000
-        self.writer_min_rows = 6000
-        self.read_packets_count_value_log_info = 10000
-        self.check_flows_ending_min_flows = 2000
-        self.capturer_updating_flows_min_value = 2000
-        self.max_rows_number = 900000
-        self.batch_address = ""
-        self.vxlan_ip = ""
-        self.continues_batch_address = ""
-        self.continues_pcap_prefix = ""
-        self.batch_address_output = ""
-        self.number_of_continues_files = 0
-        self.read_config_file()
+        self.feature_extractor_min_flows: int = 4000
+        self.writer_min_rows: int = 6000
+        self.read_packets_count_value_log_info: int = 10000
+        self.check_flows_ending_min_flows: int = 2000
+        self.capturer_updating_flows_min_value: int = 2000
+        self.max_rows_number: int = 900000
+        self.config_file_address: str = config_file_address
+        if config_file_address:
+            self.read_config_file()
+        self.pcap_file_address: str = pcap_file_address
+        self.output_file_address: str = output_file_address
+        
+        if self.pcap_file_address is None and self.batch_address is None and self.continues_batch_address is None:
+            raise Exception("Please specify the 'pcap_file_address' or 'batch_address' or 'continues_batch_address' in the config file.")
 
     def read_config_file(self):
         try:
             with open(self.config_file_address) as config_file:
                 for key, value in json.loads(config_file.read()).items():
                     setattr(self, key, value)
-                if self.pcap_file_address is None and self.batch_address is None and self.continues_batch_address is None:
-                    raise Exception("Please specify the 'pcap_file_address' or 'batch_address' or 'continues_batch_address' in the config file.")
         except Exception as error:
-            print(f">> Error was detected while reading {self.config_file_address}: {str(error)}. "\
-                    "Default values will be applied.")
+            print(f">> Error was detected while reading {self.config_file_address}: {str(error)}. ")
             exit(-1)
